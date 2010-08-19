6185a3c822513aadaa6b8efc3119bfb862978952
Constantin <constantin@web.(none)>
2010-08-19 14:40:12 +0400

    initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyjsonrpc2.0',
+    version='2.0',
+    description='Python jsonRPC client/server library',
+    author='STM',
+    author_email='c.slednev@gmail.com',
+    py_modules=['pyjsonrpc2'],
+    url='http://github.com/unk2k/pyjsonrpc2.0',
+)
+
